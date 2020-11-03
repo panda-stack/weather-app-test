3dd84b157bdeada10997afa16a935d80acd2e9bd
panda <kongfupanda2050@gmail.com>
2020-11-03 11:33:49 -0800

    built original app

---
+++ src/components/BigLabel.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const BigLabel = styled.h2`
+  color: ${({ color }) => color || '#FFFFFF'};
+  display: block;
+  font-weight: ${({ weight }) => weight || '600'};
+  font-size: ${({ fontSize }) => fontSize || '30px'};
+  text-align: ${({ align }) => align || 'left'};
+  padding: 5px 0;
+  ${({ firstToUpperCase }) =>
+    firstToUpperCase &&
+    `
+  &:first-letter {
+    text-transform: uppercase;
+  }
+  `}
+`;
+
+export default BigLabel;
